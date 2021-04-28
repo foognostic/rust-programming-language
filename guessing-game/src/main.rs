e2398f6ea0299b1555b822f8cbe2c943ebe75395
foognostic <seth@foognostic.sh>
2021-04-28 17:16:12 +0000

    It runs!

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,11 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Please input your guess: ");
+    let secret_number = rand::thread_rng().gen_range(1, 10);
 
-    let mut guess = String::new();
+    println!("the secret number is {}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("womp");
+    loop {
+        println!("Please input your guess: ");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("womp");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("that's not a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("the same");
+                break;
+            }
+        }
+    }
 }
